dc0f2f44d32bd14e437a428d2b2084379b5c90f0
Zach Desai - Intern <Zach.Desai@bentley.com>
2019-06-24 12:57:23 -0500

    working on select project option

--- src/frontend/components/Group.tsx
+++ src/frontend/components/Group.tsx
@@ -7,7 +7,7 @@ import * as React from "react";
 import iModelDataWidget from "./iModelList";
 import "../../common/configuration.js";
 import "./Group.scss";
-import { Button, ButtonType } from "@bentley/ui-core";
+// import { Button, ButtonType } from "@bentley/ui-core";
 import { Config } from "@bentley/imodeljs-clients";
 
 // WIP, event emitter bound function to be called from App.tsx and create changes in the AppState
@@ -52,12 +52,79 @@ export const GroupWidget = () => {
     <div>
       <link rel='stylesheet' href="./Group.scss" type='text/css' />
       <div className="midLeft">
-        <Button id="submitt" buttonType={ButtonType.Primary} name="submit" value="Submit">Submit</Button>
+        {/* <Button id="submitt" buttonType={ButtonType.Primary} name="submit" value="Submit">Submit</Button> */}
+        <ProjectList />
         <IModelList />
       </div>
     </div>
   );
 };
+
+/* IProjetcList class ,is a react component, has an app state with defined instance variables to keep track of relevant information */
+export class ProjectList extends React.Component<{}, { value: string }> {
+  public myRef: HTMLElement | undefined;
+  public prevIndex: number | undefined;
+  constructor(props: any) {
+    super(props);
+    this.state = { value: '' };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  // WIP, handles changes to the state of this piece when the desired project is changed, functionality not yet implemented
+  handleChange(event: Event) {
+    if (event.target)
+      this.setState({});
+  }
+
+  // Handles new form submission of dropdown elements from the list
+  handleSubmit() {
+
+    // alert('A name was submitted: ' + this.state);
+    // gets the document list and casts it to a select element
+    var mainList = (document.getElementById("dropList")) as HTMLSelectElement;
+
+    // This conditional holds code that keeps track of a variety of things. 1) The index of the option previously chosen, 2) the object currently selected becomes the selected
+    // object, 3) the default node's inner HTML is updated, so the current node is displayed even when the dropdown tool is closed, 4) begins changing the Config files and sending an event that will
+    // change the App State, changing the App State is necessary, as it will force react to re-render the desired processes, providing a new view.
+    if (mainList) {
+      console.log("selected index " + mainList.selectedIndex);
+      mainList.options[mainList.selectedIndex].selected = true;
+      if (this.prevIndex) {
+        mainList.options[this.prevIndex].selected = false;
+        this.prevIndex = mainList.selectedIndex;
+      } else {
+        this.prevIndex = mainList.selectedIndex;
+      }
+
+      // updates the primary node of the select element
+      mainList.options[0].innerHTML = mainList.options[mainList.selectedIndex].innerText;
+
+      // this config setting line should be changed, as we transition to reading and writing from a JSON file for smoother integration
+      Config.App.set("imjs_test_project", mainList.options[mainList.selectedIndex].innerText);
+      IModelContainer.setNewIModel(mainList.options[mainList.selectedIndex].innerText);
+
+      // stores an IModelData object representing the imodel selected
+      IModelContainer.iModelObject = {
+        iModelName: mainList.options[mainList.selectedIndex].innerHTML,
+        iModelValue: mainList.options[mainList.selectedIndex].value,
+      };
+    }
+  }
+
+  // render method, called automatically by react, provides formatting in HTML for the group tool widget
+  render() {
+    return (
+      <form onSubmit={() => this.handleSubmit}>
+        <label className="label">
+          <select id="dropList" name="ProjectList" style={{ fontFamily: "sans-serif" }} value="List of Projects" onChange={() => { this.handleSubmit() }}>{iModelDataWidget().map((iModelItem) => {
+            return <option key={iModelItem.key} /*onClick={() => this.handleSubmit()}*/ value={iModelItem.iModelValue}>{iModelItem.iModelName}</option>;
+          })}List of Projects</select>
+        </label>
+      </form>
+    );
+  }
+}
 
 /* IModelList class ,is a react component, has an app state with defined instance variables to keep track of relevant information */
 export class IModelList extends React.Component<{}, { value: string }> {
