c86f107cd4828d70d116b5c59d8296510df25d1e
Nicholas227 <nw101nw@gmail.com>
2019-07-15 10:55:31 -0500

    Worked with unit tests

--- test/unit/backend/config.test.ts
+++ test/unit/backend/config.test.ts
@@ -3,17 +3,34 @@ import * as fs from "fs";
 import * as electronMain from "../../../src/backend/electron/main";
 import { expect } from "chai";
 import * as path from "path";
+import console = require("console");
 
-describe("readConfig", () => {
-  it("should return the actual values stored in the config.json", () => {
+describe("Reading from local Files", () => {
+  it("should return the actual values stored in the settings.json", () => {
     fs.readFile(path.join(__dirname, "../../../lib/common/settings.json"), (error: Error | null, data: any) => {
       if(error){
         console.log("");
       }
       const jsonObject = JSON.parse(data);
       const actualJSONObject = electronMain.readData();
+      if(actualJSONObject) {
       expect(actualJSONObject).to.be.equal(jsonObject);
+      }
     });
+  });
+});
 
+describe("Editting Local Files", () => {
+  it("should correctly write strings to JSON file", () => {
+    electronMain.changeiModel("Config_Test");
+    fs.readFile(path.join(__dirname, "../../../lib/common/settings.json"), (error: Error | null, data: any) => {
+      if(error) {
+        console.log(error);
+      }
+      const jsonObject = JSON.parse(data);
+      if (jsonObject) {
+        expect(jsonObject.imodel_name).to.be.equal("Config_Test");
+      }
+    });
   });
 });
