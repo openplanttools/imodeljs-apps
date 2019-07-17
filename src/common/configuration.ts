3a784c751a71fcca2b4391ff7148ed4559d19e85
Nicholas227 <nw101nw@gmail.com>
2019-07-17 09:51:03 -0500

    Testing

--- src/common/configuration.ts
+++ src/common/configuration.ts
@@ -49,18 +49,18 @@ export default function setupEnv() {
     // * CONFIGURATION SETTINGS: *
 
     imjs_browser_test_redirect_uri: "electron://frontend/signin-callback",
-    imjs_browser_test_client_id: "spa-TndE76P2OLOLZMumh7nx5yHjR", // Un-comment if in Production
-    // imjs_browser_test_client_id: "spa-r2eFZFCkkth19yr8R2Ei4oTk1", // Un-comment if in QA
-    //imjs_buddi_resolve_url_using_region: "102", // Un-comment if in QA
+   //imjs_browser_test_client_id: "spa-TndE76P2OLOLZMumh7nx5yHjR", // Un-comment if in Production
+    imjs_browser_test_client_id: "spa-r2eFZFCkkth19yr8R2Ei4oTk1", // Un-comment if in QA
+    imjs_buddi_resolve_url_using_region: "102", // Un-comment if in QA
 
     // imjs_test_project: "opbimdev01", // Set this to the name of the Project
     // imjs_test_imodel: "OpDev_IPL_Baytown_YII", // Set this to the name of the iModel
     // imjs_test_drawing: "PIDP301", // Set this to the name of the Drawing
 
     // imjs_test_imodel: "Nick-May2019",
-     imjs_test_project: "OP_CE_VIEW",
-     imjs_test_imodel: "MistyMountainTop",
-     imjs_test_drawing: "Pid001_MT",
+    //  imjs_test_project: "OP_CE_VIEW",
+    //  imjs_test_imodel: "MistyMountainTop",
+    //  imjs_test_drawing: "Pid001_MT",
 
     // default permissions that allow the application to function
     imjs_browser_test_scope: "openid email profile organization imodelhub context-registry-service reality-data:read imodeljs-router",
--- src/frontend/components/iModelList.tsx
+++ src/frontend/components/iModelList.tsx
@@ -3,10 +3,10 @@
 * Licensed under the MIT License. See LICENSE.md in the project root for license terms.
 *--------------------------------------------------------------------------------------------*/
 import { getIModelsList } from "../../backend/electron/main.js";
-import { Config, HubIModel } from "@bentley/imodeljs-clients";
-import * as configurationData from "../../common/settings.json";
+import { HubIModel } from "@bentley/imodeljs-clients";
+import { ipcRenderer } from "electron";
 // tslint:disable: no-console
-
+export var correctIModelName: string;
 // This method initializes the data structure that will store the the iModelData objects, it returns both a test data array, and an empty real data array
 const createiModelStorage = () => {
   const iModelData = [];
@@ -23,33 +23,16 @@ const createiModelStorage = () => {
     iModelValue: "Testing 2",
     key: "Testing 2",
   };
+  getCorrectiModelName();
   const defaultData = {
-    iModelName: getCorrectiModelName(),
-    iModelValue: getCorrectiModelName(),
-    key: Config.App.get("imjs_test_imodel"),
+    iModelName: correctIModelName,
+    iModelValue: correctIModelName,
+    key: correctIModelName,
   };
   iModelData.push(defaultData);
   testData.push(testData1);
   testData.push(testData2);
   return iModelData;
-};
-
-export const getCorrectProjectName = () => {
-  if (configurationData.project_name.length > 0 && configurationData.imodel_name.length > 0) {
-    return configurationData.project_name;
-  }
-  return Config.App.get("imjs_test_project") as string;
-};
-
-export const getCorrectiModelName = () => {
-  const newJSON = configurationData as any as any[];
-  console.log(newJSON);
-  console.log(newJSON[0]);
-  if (configurationData.imodel_name.length > 0) {
-    console.log("returning configuration JSON name");
-    return configurationData.imodel_name;
-  }
-  return Config.App.get("imjs_test_imodel") as string;
 };
 
 /* Creates an iModel data widget, the translates data from the iModel hub, and stores it as an array of iModelData objects */
@@ -79,4 +62,27 @@ const iModelDataWidget = () => {
   return iModelData;
 };
 
-export default iModelDataWidget;
+export const getCorrectiModelName = () => {
+
+  // Sets up listener for response back from server
+  ipcRenderer.on("readConfigResultsIModel", (event: Event, jsonObject: any) => {
+    if (event) {
+      console.log(jsonObject);
+    }
+
+    // Configures the correct value, setting the state of the app, depending on what values currently exist
+    // values in the settings.json are prioritized
+    let configiModel = jsonObject.imodel_name;
+    if (jsonObject.imodel_name.length < 1) {
+      alert("Warning! Invalid settings, missing imodel name");
+      try {
+        throw new ReferenceError("No imodel id has been specified");
+      } catch (e) {
+        console.log((e as Error).message);
+        ipcRenderer.send("closeApplication", "Missing imodel");
+      }
+    }
+    correctIModelName = configiModel;
+  })};
+
+  export default iModelDataWidget;
